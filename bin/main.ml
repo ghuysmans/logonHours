0fe1582824e6250a9067b346b47e71d2aca4698d
Guillaume Huysmans <ghuysmans99@gmail.com>
2021-09-25 16:43:52 +0200

    Generalize dump

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,9 @@
 open LogonHours
 
-let dump l =
+let dump lang l =
+  print_endline "    0123456789AB0123456789AB";
   Day.european |> List.iter (fun d ->
-    print_string (Day.to_string d);
+    print_string (lang d);
     print_char ' ';
     for h = 0 to 23 do
       print_char (if l.(Day.to_int d * 24 + h) then '1' else '0')
@@ -15,7 +16,7 @@ let () =
   let bias = 1 in
   let w = of_string ~bias (really_input_string stdin 21) in
   let l = w#to_local in
-  dump l;
+  dump Day.to_string l;
   let d = Day.of_string Sys.argv.(1) in
   let h = int_of_string Sys.argv.(2) in
   if w#get d h then
