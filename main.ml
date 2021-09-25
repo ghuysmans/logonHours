eb75b4ba89dd03cb0d3ea25890502e3e08b267e1
Guillaume Huysmans <ghuysmans99@gmail.com>
2021-09-25 03:01:13 +0200

    Extract iter

--- main.ml
+++ main.ml
@@ -1,22 +1,5 @@
 open Raw
 
-let to_local raw bias =
-  let local = Array.make (7 * 3 * 8) false in
-  let l = ref bias in
-  let i = ref 0 in
-  for _day = 0 to 6 do
-    for _eight_hours = 0 to 2 do
-      let m = ref 1 in
-      while !m <= 128 do
-        local.(!l) <- raw.(!i) land !m <> 0;
-        incr l; if !l >= 7 * 24 then l := !l - 7 * 24; (* FIXME? *)
-        m := !m lsl 1
-      done;
-      incr i; (* next input byte *)
-    done
-  done;
-  local
-
 let dump l =
   for d = 0 to 6 do
     print_int d;
@@ -29,7 +12,8 @@ let dump l =
 
 
 let () =
-  let raw = [|
+  let bias = 1 in
+  let w = new wrapper bias [|
     0; 0; 0;
     0b10000000; 0b11111111; 0b00000000;
     0b10000000; 0b11111111; 0b00000000;
@@ -38,12 +22,11 @@ let () =
     0b10000000; 0b11111111; 0b00000000;
     0; 0; 0;
   |] in
-  let bias = 1 in
-  let l = to_local raw bias in
+  let l = w#to_local in
   dump l;
   let d = Day.of_string Sys.argv.(1) in
   let h = int_of_string Sys.argv.(2) in
-  if (new wrapper bias raw)#get d h then
+  if w#get d h then
     print_endline "allowed"
   else
     print_endline "denied"
--- raw.ml
+++ raw.ml
@@ -13,7 +13,7 @@ let locate ~bias d h =
   in
   {index = d * 3 + h / 8; mask = 1 lsl (h land 7)}
 
-class wrapper bias raw = object
+class wrapper bias raw = object(self)
   method array = raw
 
   method get d h =
@@ -27,4 +27,26 @@ class wrapper bias raw = object
   method clear d h =
     let {index; mask} = locate ~bias d h in
     raw.(index) <- raw.(index) land (lnot mask)
+
+  method iter f =
+    let i = ref 0 in
+    for _day = 0 to 6 do
+      for _eight_hours = 0 to 2 do
+        let m = ref 1 in
+        while !m <= 128 do
+          f (raw.(!i) land !m <> 0);
+          m := !m lsl 1
+        done;
+        incr i
+      done
+    done
+
+  method to_local =
+    let local = Array.make (7 * 3 * 8) false in
+    let i = ref bias in
+    self#iter (fun allowed ->
+      local.(!i) <- allowed;
+      incr i; if !i >= 7 * 24 then i := !i - 7 * 24
+    );
+    local
 end
